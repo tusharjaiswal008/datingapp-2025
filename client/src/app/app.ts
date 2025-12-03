ee538d84c7f9619c53285213b34992bf1e83bd3b
Tushar Jaiswal <tusharjaiswal008@gmail.com>
2025-12-03 20:10:48 +0530

    End of Section 3(Walking Skeleton - 2)

---
+++ client/src/app/app.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+
+})
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  protected members = signal<any>([]);
+
+    ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response=>this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    });
+  }
+}
